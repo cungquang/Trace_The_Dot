073e78911dcbd524f8cd2831af725fd881308d4c
cungquang <cungquang1987@gmail.com>
2024-04-02 11:22:25 -0700

    update testing

--- app/src/main.c
+++ app/src/main.c
@@ -1,10 +1,6 @@
-#include "./neoPixelRGBW-Linux.h"
+#include "../include/neoPixelRGBW-Linux.h"
+#include "../include/joystick-Linux.h"
 #include "../include/i2cbus1_control.h"
-#include "../include/general_helper.h"
-#include "../../hal/include/i2c_configure.h"
-
-#define P8_11_STATE_PATH "/sys/devices/platform/ocp/ocp:P8_11_pinmux/state"
-#define CONFIGURE_PIN_811 "config-pin P8.11 pruout > /dev/null"
 
 #define CONFIGURE_PIN_815 "config-pin p8_15 pruin"
 #define CONFIGURE_PIN_816 "config-pin p8_16 pruin"
@@ -16,5 +12,12 @@ int main(void)
     // I2cbus1Control_join();
     // neoPixel_cleanup();
 
-    
+    joystick_init();
+    printf("Down is press: %d\n", joystickDown_isPressed());
+    printf("Down is Count: %d\n", joystickDown_pressCount());
+    printf("Right is press: %d\n", joystickRight_isPressed());
+    printf("Right is Count: %d\n", joystickRight_pressCount());
+    joystickDown_pressCount();
+
+    joystick_cleanup();
 }
