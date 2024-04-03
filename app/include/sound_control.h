31709957919cef250f81bdbf40ee0bef8d786428
cungquang <cungquang1987@gmail.com>
2024-04-03 15:13:41 -0700

    update sound

---
+++ app/include/sound_control.h
@@ -0,0 +1,9 @@
+#ifndef _SOUND_CONTROL_H_
+#define _SOUND_CONTROL_H_
+
+#include <stdio.h>
+#include <pthread.h>
+
+void Sound_init(void);
+
+#endif
--- app/src/sound_control.c
+++ app/src/sound_control.c
@@ -1,18 +1,79 @@
+#include "../include/general_helper.h"
+#include "../include/sound_control.h"
 #include "../../hal/include/pwm_configure.h"
 
-static int hit_count;
+//Control operation
+static int isTerminate;
+
+//Status to trigger play sound
+static int playHitTarget;
+static int playMissTarget;
+
+//Threads
+static pthread_t sound_playTargetSoundThreadId;
+
+//Mutex
+static pthread_mutex_t shared_pipe_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 
 //Initiate private function
-static void Sound_init(void);
+static void * Sound_playTargetSound();
 static void Sound_playHitTarget(void);
 static void Sound_playMissTarget(void);
 
 
-static void Sound_init(void)
+/*
+#########################
+#        PUBLIC         #
+#########################
+*/
+
+void Sound_init(void)
 {
     //Configure the pin
     Pwm_init();
+
+    //Create thread
+    pthread_create(&sound_playTargetSoundThreadId, NULL, Sound_playTargetSound, NULL);
 }
+
+void Sound_join(void)
+{
+    pthread_join(&sound_playTargetSoundThreadId, NULL);
+}
+
+void Sound_setTerminate(void)
+{
+    isTerminate = 1;
+}
+
+/*
+#########################
+#        PRIVATE        #
+#########################
+*/
+
+
+static void * Sound_playTargetSound()
+{
+    while(!isTerminate)
+    {
+        if(playHitTarget)
+        {
+            Sound_playHitTarget();
+        }
+        else if(playMissTarget)
+        {
+            Sound_playMissTarget();
+        }
+
+        // Delay before recheck
+        sleepForMs(200);
+    }
+
+    return NULL;
+}
+
 
 static void Sound_playHitTarget(void)
 {
